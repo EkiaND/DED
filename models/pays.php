3c40549808d12d841aab6ac15279783b834c8bfb
Anthony Yon <tony.yon@outlook.fr>
2025-03-17 08:10:25 +0100

    Réalisation de la fonction getPaysParRegion

--- models/pays.php
+++ models/pays.php
@@ -1,3 +1,20 @@
 <?php
 // Fichier pour gérer les entités pays
+
+function getPaysParRegion($region){
+	$conn = getBDD();
+	$pays = null;
+	$req = "SELECT nom_pays FROM pays as p JOIN region as r ON r.id_region = p.id_region WHERE region = ?;";
+	$stmt = mysqli_prepare($conn,$req);
+	if ($stmt === false) {
+		echo mysqli_error($conn);
+	}
+	else{
+		mysqli_stmt_bind_params($stmt,"s",$region);
+        if (mysqli_stmt_execute($stmt)){
+            $res = mysqli_stmt_get_result($stmt);
+            return mysqli_fetch_assoc($res,MYSLI_ASSOC);
+        }
+	}
+}
 ?>
