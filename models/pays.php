78999c1c22d4ce53ea24dc1ad1f3054d191b60e7
Anthony Yon <tony.yon@outlook.fr>
2025-03-22 12:01:39 +0100

    fonction récupérer liste pays dans pays.php
    j'ai décidé de prendre aussi le code_pays. Dites moi ce que vous en pensez.

--- models/pays.php
+++ models/pays.php
@@ -1,6 +1,29 @@
 <?php
 // Fichier pour gérer les entités pays
 
+
+//Récupérer tout les pays
+function getPays(){
+	
+	$conn = getBDD();
+	$pays = [];
+	$req = "SELECT code_pays, nom_pays FROM jouets;";
+
+	$stmt = mysqli_prepare($conn,$req);
+
+	if( $stmt === false){
+		echo mysqli_error($conn);
+	}
+	else {
+		if (mysqli_stmt_execute($stmt)){
+            $res = mysqli_stmt_get_result($stmt);
+            return mysqli_fetch_assoc($res,MYSLI_ASSOC);
+		}
+	}	
+}
+
+
+// Récupérer pays par région
 function getPaysParRegion($region){
 	$conn = getBDD();
 	$pays = null;
