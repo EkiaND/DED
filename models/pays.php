a7d5235fb2a279ab7f6304f0444198039d28fd2e
Anthony Yon <tony.yon@outlook.fr>
2025-03-22 13:34:22 +0100

    Update pays.php
    Rectification fonction getPays() et création de la fonction getDetailsPays()

--- models/pays.php
+++ models/pays.php
@@ -7,7 +7,7 @@ function getPays(){
 	
 	$conn = getBDD();
 	$pays = [];
-	$req = "SELECT code_pays, nom_pays FROM jouets;";
+	$req = "SELECT code_pays, nom_pays FROM pays;";
 
 	$stmt = mysqli_prepare($conn,$req);
 
@@ -38,6 +38,27 @@ function getPaysParRegion($region){
             $res = mysqli_stmt_get_result($stmt);
             return mysqli_fetch_assoc($res,MYSLI_ASSOC);
         }
+	}
+}
+
+//Récupérer les détails sur les pays
+function getDetailsPays($nomPays){
+	
+	$conn = getBDD();
+	$pays = [];
+	$req = """ SELECT p.nom_pays, p.code_pays, r.nom_region, p.groupe_revenu FROM pays as p 
+			   JOIN regions as r ON r.id_region = p.id_region WHERE p.nom_pays = ? ;";
+
+	$stmt = mysqli_prepare($conn,$req);
+	if ($stmt === false) {
+		echo mysqli_error($conn);
+	}
+	else{
+		mysqli_stmt_bind_params($stmt,"s",$nomPays);
+		if (mysqli_stmt_execute($stmt)){
+			$res = mysqli_stmt_get_result($stmt);
+			return mysqli_fetch_assoc($res,MYSLI_ASSOC);
+		}
 	}
 }
 ?>
