dc9d37e866927aa2f05fcfe9e1fc2bfaaa213fb0
Anthony Yon <tony.yon@outlook.fr>
2025-03-29 18:03:46 +0100

    Update indicateurs.php (controller)
    Création fonction qui récupère le PIB Mondial par années

--- controllers/indicateurs.php
+++ controllers/indicateurs.php
@@ -1,3 +1,40 @@
 <?php
 // Fichier pour le contrôleur principal des indicateurs
+require_once __DIR__ . '/../models/indicateur.php';
+require_once __DIR__ . '/../models/pays.php';
+
+
+ // Récupérer le PIB Mondial par année
+function getMoyennePIBMondial() {
+    $pays = getPays(); 
+    $pibParAnnee = [];
+
+    foreach ($pays as $paysInfo) {
+        $codePays = $paysInfo['code_pays'];
+        $valeurs = getValeursIndicateur('pib', $codePays);
+
+        foreach ($valeurs as $data) {
+            $annee = $data['annee'];
+            $pib = $data['valeur'];
+
+            if (!isset($pibParAnnee[$annee])) {
+                $pibParAnnee[$annee] = ['total' => 0, 'nb_pays' => 0];
+            }
+
+            if (!is_null($pib) && $pib > 0) {
+                $pibParAnnee[$annee]['total'] += $pib;
+                $pibParAnnee[$annee]['nb_pays']++;
+            }
+        }
+    }
+
+    $moyennePIB = [];
+    foreach ($pibParAnnee as $annee => $data) {
+        if ($data['nb_pays'] > 0) {
+            $moyennePIB[$annee] = $data['total'] / $data['nb_pays'];
+        }
+    }
+
+    return $moyennePIB; 
+}
 ?>
