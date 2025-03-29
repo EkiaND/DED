6199402b7b297c35c352fe2850e774e5934135c9
Anthony Yon <tony.yon@outlook.fr>
2025-03-29 18:02:58 +0100

    Update index.php
    Include selecteur.php et header.php
    Formatage avec style.css

--- index.php
+++ index.php
@@ -19,35 +19,13 @@
     <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="index.php?url=dashboard">Tableau de bord</a></li>
-            <li><a href="index.php?url=infos_pays">Informations sur les pays</a></li>
-            <li><a href="index.php?url=comparaison_pays">Comparaison des pays</a></li>
-        </ul>
-    </nav>
 
     <?php
-    // Point d'entrée de l'application
-
-    // Récupérer l'URL demandée
-    $url = isset($_GET['url']) ? $_GET['url'] : '';
-
-    // Charger les fichiers en fonction de l'URL
-    switch ($url) {
-        case 'dashboard':
-            include 'views/dashboard.php';
-            break;  
-        case 'infos_pays':
-            include 'views/infos_pays.php';
-            break;
-        case 'comparaison_pays':
-            include 'views/comparaison_pays.php';
-            break;
-        default:
-            include 'views/dashboard.php';
-            break;
-    }
+    include('views/partials/header.php'); // Inclusion du header
+    include('views/partials/selecteurs.php'); // Inclusion du sélecteur
     ?>
+    
+
+    
 </body>
 </html>
