17145309fa18e78c0186d9c0d8856b107ab1c039
Ekia. <144850026+EkiaND@users.noreply.github.com>
2025-04-09 16:53:23 +0200

    Delete views/indicateur_pays.php

--- views/indicateur_pays.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Vue pour la partie B
-echo 'Informations sur un pays';
-?>
-
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="./public/js/graphiques.js"></script>
